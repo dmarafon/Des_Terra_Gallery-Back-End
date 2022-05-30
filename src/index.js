117c69010607b5cbf6f319d16328f42b98fa0914
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-30 17:29:31 +0200

    Adding Errors middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const connectDB = require("./database");
 const initializeServer = require("./server/initializeServer");
 
 const port = process.env.PORT ?? 4000;
-const connectionString = process.env.MONGO_CONNECTION;
+const connectionString = process.env.MONGO_STRING;
 
 (async () => {
   try {
