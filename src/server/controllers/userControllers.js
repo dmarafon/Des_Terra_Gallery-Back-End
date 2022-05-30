100d0171952e1220ba674bc576575a46215c227c
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-30 18:17:46 +0200

    Adding user controllers for login

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+const debug = require("debug")("desterra:server:middlewares:errors");
+const User = require("../../database/models/User");
+
+const loginUser = async (req, res, next) => {
+  const username = req.body.username.toString();
+  const password = req.body.password.toString();
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    const error = new Error("Incorrect password");
+    error.statusCode = 403;
+    error.customMessage = "Username or password is wrong";
+
+    next(error);
+  } else {
+    const userData = {
+      name: user.name,
+      username: user.username,
+      id: user.id,
+    };
+    const rightPassword = await bcrypt.compare(password, user.password);
+
+    if (!rightPassword) {
+      const error = new Error("Incorrect password");
+      error.statusCode = 403;
+      error.customMessage = "Username or password is wrong";
+
+      next(error);
+    } else {
+      const token = jsonwebtoken.sign(userData, process.env.JWT_SECRET_USER);
+
+      res.status(200).json({ token });
+    }
+  }
+};
+
+module.exports = { loginUser };
