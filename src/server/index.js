3d917cc1c7906028998fbbb2432b70e11e7f3e6b
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-30 17:36:59 +0200

    Fixing security issues

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const { notFoundError, generalError } = require("./middlewares/errors");
+
+const corsOptions = {
+  origin: [
+    "http://localhost:4000",
+    "http://localhost:4001",
+    "http://localhost:4002",
+    "http://localhost:5000",
+    "http://localhost:5001",
+    "http://localhost:5002",
+    "https://daniell-marafon-front-final-project-202204-bcn.netlify.app/",
+    "https://daniell-marafon-front-final-project-202204-bcn.netlify.app",
+  ],
+};
 
 const app = express();
+
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(helmet());
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
