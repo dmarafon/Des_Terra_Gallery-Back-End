802db9c6b915e9ff68d7d4fcfee1964b4747baa0
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-30 20:48:09 +0200

    Adding Joi validation for login

---
+++ src/utils/schemas/userCredentialsSchema.js
@@ -0,0 +1,17 @@
+const { Joi } = require("express-validation");
+
+const credentialsLoginSchema = {
+  body: Joi.object({
+    email: Joi.string()
+      .email({ tlds: { allow: false } })
+      .max(254)
+      .messages({ message: "An email is Required" })
+      .required(),
+    password: Joi.string()
+      .max(15)
+      .messages({ message: "A Password is Required" })
+      .required(),
+  }),
+};
+
+module.exports = { credentialsLoginSchema };
