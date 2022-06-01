c3bda874d9e6955cbd0a845ee0e10ca1ced53005
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-01 09:46:51 +0200

    Adding Joi for register

--- src/utils/routers/userRouters.js
+++ src/utils/routers/userRouters.js
@@ -1,11 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const { validate } = require("express-validation");
-const { loginUser } = require("../../server/controllers/userControllers");
-const { credentialsLoginSchema } = require("../schemas/userCredentialsSchema");
+const multer = require("multer");
+const path = require("path");
+const {
+  loginUser,
+  registerUser,
+} = require("../../server/controllers/userControllers");
+const {
+  credentialsLoginSchema,
+  credentialsRegisterSchema,
+} = require("../schemas/userCredentialsSchema");
 
 const usersRouter = express.Router();
 
+const upload = multer({
+  dest: path.join("uploads", "images"),
+});
+
 usersRouter.post("/login", validate(credentialsLoginSchema), loginUser);
+
+usersRouter.post(
+  "/register",
+  upload.single("image"),
+
+  validate(credentialsRegisterSchema),
+  registerUser
+);
 
 module.exports = usersRouter;
