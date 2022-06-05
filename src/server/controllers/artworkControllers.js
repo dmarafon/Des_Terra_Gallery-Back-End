57c931d8f5f4d4e8d022e3cf2c983333f9497825
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-05 20:58:00 +0200

    Adding populate to get author name

--- src/server/controllers/artworkControllers.js
+++ src/server/controllers/artworkControllers.js
@@ -1,10 +1,16 @@
 const Artwork = require("../../database/models/Artwork");
+const User = require("../../database/models/User");
 const customError = require("../../utils/customError");
 
 const getPaginatedArtworks = async (req, res, next) => {
   const { page = 1, limit = 10 } = req.query;
   try {
     const artworks = await Artwork.find()
+      .populate({
+        path: "author",
+        select: "firstname surname",
+        model: User,
+      })
       .limit(limit * 1)
       .skip((page - 1) * 1);
 
