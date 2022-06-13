b53c7a2b35ee96c4a7eeac7525902e438ae3e25d
Daniell Marafon <daniellmarafon@gmail.com>
2022-06-13 15:25:26 +0200

    Adding another test suit, for get myartwork contrioller

--- src/server/controllers/artworkControllers.js
+++ src/server/controllers/artworkControllers.js
@@ -115,7 +115,7 @@ const getPaginatedArtworks = async (req, res, next) => {
 };
 
 const getPaginatedMyArtworks = async (req, res, next) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 12 } = req.query;
   const { userId } = req;
 
   try {
